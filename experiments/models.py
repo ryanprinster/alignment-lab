da06b12d2b7af6995e26711995cf1968be1ab05e
Ryan Prinster <ryanprinster@Ryans-MacBook-Air-2.local>
2025-10-28 13:53:03 -0700

    tweaks

--- experiments/models.py
+++ experiments/models.py
@@ -170,11 +170,11 @@ class Llama_3p2_1B_Causal(Llama_3p2_1B):
             output_attentions=False,
             use_cache=False 
         )
+        pdb.set_trace()
 
         if max_query_length_truncate is not None:
             return outputs.logits[:,max_query_length_truncate:,:], outputs.loss
         
-        pdb.set_trace()
         return outputs.logits, outputs.loss
 
     def _set_model_class(self):
