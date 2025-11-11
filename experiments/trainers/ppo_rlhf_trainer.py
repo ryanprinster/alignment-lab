a6514c63e1cfb7f689f69ae188b8e8e971d6387c
Ryan Prinster <ryanprinster@Ryans-MacBook-Air-2.local>
2025-11-11 15:34:54 -0800

    tweaks

--- experiments/trainers/ppo_rlhf_trainer.py
+++ experiments/trainers/ppo_rlhf_trainer.py
@@ -188,7 +188,6 @@ class PPORLHFTrainer(BaseTrainer):
                             # 2.2 Compute ppo loss for policy model
                             # NOTE: all tensors in function below are in fp32?
                             loss_ppo, entropy = self.compute_policy_loss_ppo(old_actions, old_log_probs, A, new_log_policies, pad_mask)
-                            pdb.set_trace()
 
                             del new_log_policies
 
