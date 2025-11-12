f40ebd694ba66a5917cbaf994606c31281c85eb8
Ryan Prinster <ryanprinster@Ryans-MacBook-Air-2.local>
2025-11-12 11:24:46 -0800

    tweaks

--- experiments/environment.py
+++ experiments/environment.py
@@ -278,6 +278,7 @@ class RLHFEnvironment(BaseEnvironment):
                 rewards = whiten(rewards, shift_mean=False)
             
             # rewards = whiten(r) - beta * kl
+            pdb.set_trace()
             rewards -= self.config.beta * tj.kl
             tj.compute_R(gamma=self.config.gamma, r=rewards)
 
