d261cf8cc1a0610be7ac2dbf8d81c244fc5eee75
Ryan Prinster <ryanprinster@Ryans-MacBook-Air-2.local>
2025-11-19 14:42:26 -0800

    tweaks

--- experiments/trainers/ppo_rlhf_trainer.py
+++ experiments/trainers/ppo_rlhf_trainer.py
@@ -105,7 +105,7 @@ class PPORLHFTrainer(BaseTrainer):
         
         r = torch.exp((new_log_probs - old_log_probs).masked_fill(~mask, 0))
 
-        pdb.set_trace()
+        # pdb.set_trace()
 
         # Compute ppo loss
         loss_ppo = torch.min(r * A, torch.clamp(r, 1-self.config.eps_policy_clipping , 1+self.config.eps_policy_clipping) * A)
