2033f0d327cd24f33d6ea02285685e7840ceaa27
Ryan Prinster <ryanprinster@Ryans-MacBook-Air-2.local>
2025-12-13 19:00:42 -0800

    tweaks

--- experiments/trainers/ppo_rlhf_eval.py
+++ experiments/trainers/ppo_rlhf_eval.py
@@ -59,10 +59,13 @@ class PPORLHFEval(BaseTrainer):
         #         self.device
         #     )
 
-        self.model = HFModel_Policy.from_pretrained(
-            config=self.config,
-            model_name="vwxyzjn/EleutherAI_pythia-1b-deduped__ppo_left_padding_new_nowhiten_reward__tldr",
-            revision="ppo_left_padding_new_nowhiten_reward__44413__1709671965").to(self.device)
+        # self.model = HFModel_Policy.from_pretrained(
+        #     config=self.config,
+        #     model_name="vwxyzjn/EleutherAI_pythia-1b-deduped__ppo_left_padding_new_nowhiten_reward__tldr",
+        #     revision="ppo_left_padding_new_nowhiten_reward__44413__1709671965").to(self.device)
+
+        self.model = Llama_3p2_1B_SFT(self.config, init_model_path=self.config.sft_model_path).to(self.device).requires_grad_(False)
+
 
         self.data = TLDRFilteredDataPPO(tokenizer=self.model.tokenizer, batch_size=self.config.batch_size)
        
@@ -158,7 +161,7 @@ class PPORLHFEval(BaseTrainer):
         # pdb.set_trace()
 
         for batch_idx, batch in enumerate(self.data.validation_loader):
-
+            
             input_batch, summary_ids = self.format_batch_for_generation(batch, self.data.__class__.SFT_MAX_QUERY_LENGTH)
 
             # get prompts from batch
