188920cd05e0394a109dd6f4c29a797b37a36528
Ryan Prinster <ryanprinster@Ryans-MacBook-Air-2.local>
2025-12-17 14:23:49 -0800

    remove models_v2.py

--- experiments/config.py
+++ experiments/config.py
@@ -72,8 +72,7 @@ class RLFHCaseStudyConfig(SFTConfigBase, RMConfigBase):
         # Checkpointing
         self.save_freq_steps = 100 * self.accumulation_steps
         self.save_interval_min = 60
-        self.sft_model_path = "./checkpoints/checkpoint_best.pt"
-        self.load_checkpoint_path = "./checkpoints/checkpoint_best.pt"
+        self.sft_model_path = "checkpoints/sft_final_checkpoint.pt"
         self.rm_model_path = "checkpoints/rm_final_checkpoint_v2.pt"
         
         self.keep_last_n=2
