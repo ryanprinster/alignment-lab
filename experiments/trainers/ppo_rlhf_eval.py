a7a81ee74ac5cf9be6c23065799ecf75778b2fad
Ryan Prinster <ryanprinster@Ryans-MacBook-Air-2.local>
2025-12-17 10:19:41 -0800

    modify PPORLHFEval to use model signatures

--- experiments/trainers/ppo_rlhf_eval.py
+++ experiments/trainers/ppo_rlhf_eval.py
@@ -59,7 +59,7 @@ class PPORLHFEval(BaseTrainer):
 
         self.model = HFModel_Policy.from_pretrained(
             config=self.config,
-            hf_model_name="vwxyzjn/EleutherAI_pythia-1b-deduped__ppo_left_padding_new_nowhiten_reward__tldr",
+            model_name="vwxyzjn/EleutherAI_pythia-1b-deduped__ppo_left_padding_new_nowhiten_reward__tldr",
             revision="ppo_left_padding_new_nowhiten_reward__77713__1709671965").to(self.device)
 
         # SFT Mode
