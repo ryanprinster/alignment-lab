6be1f4d14250e1ddda2ee6c3c10a0f2880ec5d35
Ryan Prinster <ryanprinster@Ryans-MacBook-Air-2.local>
2025-12-28 15:12:23 -0700

    docs and plots

--- experiments/trainers/ppo_rlhf_eval.py
+++ experiments/trainers/ppo_rlhf_eval.py
@@ -28,12 +28,12 @@ class PPORLHFEval(BaseTrainer):
         self.client = anthropic.Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY"))
 
         # Trained PPO Model
-        self.model = (
-            HFModel_Policy.init_from_hf_pretrained(self.config)
-            .to(self.device)
-            .requires_grad_(False)
-        )
-        self.model.set_from_local_state_dict(self.config.policy_checkpoint_path)
+        # self.model = (
+        #     HFModel_Policy.init_from_hf_pretrained(self.config)
+        #     .to(self.device)
+        #     .requires_grad_(False)
+        # )
+        # self.model.set_from_local_state_dict(self.config.policy_checkpoint_path)
 
         # SFT Model
         # self.model = HFModel_SFT.init_from_hf_pretrained(self.config).to(self.device).requires_grad_(False)
@@ -44,9 +44,9 @@ class PPORLHFEval(BaseTrainer):
         #     hf_model_name="vwxyzjn/EleutherAI_pythia-1b-deduped__ppo_left_padding_new_nowhiten_reward__tldr",
         #     revision="ppo_left_padding_new_nowhiten_reward__77713__1709671965").to(self.device).requires_grad_(False)
 
-        self.data = TLDRFilteredDataPPO(
-            tokenizer=self.model.tokenizer, batch_size=self.config.batch_size
-        )
+        # self.data = TLDRFilteredDataPPO(
+        #     tokenizer=self.model.tokenizer, batch_size=self.config.batch_size
+        # )
 
 
     def _to_device(self, batch):
